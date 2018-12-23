8996b38af4f2c6c23aa748d619368ac0a7cf459b
Jason Bland <jtbland@gmail.com>
2018-12-23 01:06:58 -0800

    add streams components scaffolding

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <Link to="/">Navigate to Page One</Link>
-    </div>
-  );
-};
-
 class App extends React.Component {
   render() {
     return (
---
+++ src/components/streams/StreamEdit.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StreamEdit = () => {
+  return <div>Steam Edit</div>;
+};
+
+export default StreamEdit;
