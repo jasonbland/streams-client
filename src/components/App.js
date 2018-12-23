4042d7a8c2832849f8eb4a2a972bf5772483b333
Jason Bland <jtbland@gmail.com>
2018-12-22 21:27:22 -0800

    add Link from react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 const PageOne = () => {
   return (
     <div>
       PageOne
-      {/* bad technique */}
-      <a href="/pagetwo">Navigate to Page Two</a>
+      <Link to="/pagetwo">Navigate to Page Two</Link>
     </div>
   );
 };
@@ -15,8 +14,7 @@ const PageTwo = () => {
   return (
     <div>
       PageTwo
-      {/* bad technique */}
-      <a href="/">Navigate to Page One</a>
+      <Link to="/">Navigate to Page One</Link>
     </div>
   );
 };
