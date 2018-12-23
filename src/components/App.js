e9cb195e803dd101d771e4c1abb0b873f410e349
Jason Bland <jtbland@gmail.com>
2018-12-23 01:13:23 -0800

    wired up routes to streams components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 class App extends React.Component {
   render() {
@@ -7,8 +12,11 @@ class App extends React.Component {
       <div>
         <BrowserRouter>
           <div>
-            <Route path="/" exact component={PageOne} />
-            <Route path="/pagetwo" component={PageTwo} />
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
           </div>
         </BrowserRouter>
       </div>
