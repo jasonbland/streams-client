94534a582f82675f026be24abfbe2a92614c9598
Jason Bland <jtbland@gmail.com>
2018-12-22 21:09:47 -0800

    use anchor tag for navigation (bad)

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne
+      {/* bad technique */}
+      <a href="/pagetwo">Navigate to Page Two</a>
+    </div>
+  );
 };
 
 const PageTwo = () => {
-  return <div>PageTwo</div>;
+  return (
+    <div>
+      PageTwo
+      {/* bad technique */}
+      <a href="/">Navigate to Page One</a>
+    </div>
+  );
 };
 
 class App extends React.Component {
