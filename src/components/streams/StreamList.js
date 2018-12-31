6b771108560776712c78f6255ceb290bd9101ae5
Jason Bland <jtbland@gmail.com>
2018-12-30 17:54:53 -0800

    add renderCreate to show button for crating new stream

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
@@ -13,6 +14,18 @@ class StreamList extends React.Component {
         <div className="right floated content">
           <button className="ui button primary">Edit</button>
           <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
         </div>
       );
     }
@@ -38,6 +51,7 @@ class StreamList extends React.Component {
       <div>
         <h2>Streams List</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -45,7 +59,7 @@ class StreamList extends React.Component {
 
 const mapDispatchToProps = { fetchStreams };
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams), currentUserId: state.auth.userId };
+  return { streams: Object.values(state.streams), currentUserId: state.auth.userId, isSignedIn: state.auth.isSignedIn };
 };
 
 export default connect(
