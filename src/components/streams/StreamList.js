67f4c507e6630a9e05a70e454accb7b1ac430702
Jason Bland <jtbland@gmail.com>
2018-12-30 17:45:02 -0800

    add renderAdmin for stream owner controls

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,6 +7,12 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return <div>EDIT/DELETE</div>;
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
@@ -16,6 +22,7 @@ class StreamList extends React.Component {
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
+          {this.renderAdmin(stream)}
         </div>
       );
     });
@@ -32,8 +39,8 @@ class StreamList extends React.Component {
 }
 
 const mapDispatchToProps = { fetchStreams };
-const mapStateToProps = ({ streams }) => {
-  return { streams: Object.values(streams) };
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams), currentUserId: state.auth.userId };
 };
 
 export default connect(
