a9093289a1347279d50a3634ac08515c45010212
Jason Bland <jtbland@gmail.com>
2018-12-30 20:37:19 -0800

    refactor stream edit to use initial values

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm initialValues={this.props.stream} onSubmit={this.onSubmit} />
+      </div>
+    );
   }
 }
 
-const mapDispatchToProps = { fetchStream };
+const mapDispatchToProps = { fetchStream, editStream };
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
