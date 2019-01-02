4f0ea9ee2f47f2dde0acba95dd43fb3908cc451f
Jason Bland <jtbland@gmail.com>
2019-01-01 21:36:03 -0800

    move modal dismiss to parent div

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
   const actions = (
-    <div>
+    <React.Fragment>
       <button className="ui button negative">Delete</button>
       <button className="ui button">Cancel</button>
-    </div>
+    </React.Fragment>
   );
 
   return (
     <div>
       StreamDelete
-      <Modal title="Delete Stream" content="Are you sure you want to delete this stream?" actions={actions} />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push('/')}
+      />
     </div>
   );
 };
