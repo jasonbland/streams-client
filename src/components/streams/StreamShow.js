229f7ca71522d59faf6abdb890ab3feee2c32920
Jason Bland <jtbland@gmail.com>
2019-01-02 21:53:56 -0800

    add video element and react ref

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
@@ -16,6 +23,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%', padding: '0 10px' }} controls />
         <h1>{stream.title}</h1>
         <h5>{stream.description}</h5>
       </div>
