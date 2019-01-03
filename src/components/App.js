f447eabb7265c585cb87c49a09c47cb1410ed79b
Jason Bland <jtbland@gmail.com>
2019-01-02 20:20:08 -0800

    add id and link to stream show route

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
             <Route path="/streams/new" exact component={StreamCreate} />
             <Route path="/streams/edit/:id" exact component={StreamEdit} />
             <Route path="/streams/delete/:id" exact component={StreamDelete} />
-            <Route path="/streams/show" exact component={StreamShow} />
+            <Route path="/streams/:id" exact component={StreamShow} />
           </div>
         </Router>
       </div>
